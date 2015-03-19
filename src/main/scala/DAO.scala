4fce6d0742d68db65216b2aef8951fef78e0c3bb
pamu <nagarjuna.pamu@gmail.com>
2015-03-19 19:06:16 +0530

    users and dusers

--- src/main/scala/DAO.scala
+++ src/main/scala/DAO.scala
@@ -1,3 +1,5 @@
+import java.sql.Timestamp
+
 import scala.slick.driver.MySQLDriver.simple._
 
 /**
@@ -11,14 +13,27 @@ object DAO {
     user = Constants.user,
     password = Constants.pass)
 
-  case class User(name: String, email: String, message: String, id: Option[Long] = None)
+  case class User(name: String, email: String, message: String, timestamp: Timestamp, id: Option[Long] = None)
 
-  case class Users(tag: Tag) extends Table[User](tag, "users") {
+  class Users(tag: Tag) extends Table[User](tag, "users") {
     def name = column[String]("name", O.NotNull)
     def email = column[String]("email", O.NotNull)
+    def timestamp = column[Timestamp]("timestamp", O.NotNull)
     def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
     def * = (name, email, id.?) <> (User.tupled, User.unapply)
   }
 
+  case class DUser(name: String, email: String, timestamp: String, id: Option[Long] = None)
+
+  class DUsers(tag: Tag) extends Table[DUser](tag, "dusers") {
+    def name = column[String]("name", O.NotNull)
+    def email = column[String]("email", O.NotNull)
+    def timestamp = column[String]("timestamp", O.NotNull)
+    def id = column[Long]("id", O.PrimaryKey, O.NotNull)
+    def * = (name, email, timestamp, id.?) <> (DUser.tupled, DUser.unapply)
+  }
+
   lazy val users = TableQuery[Users]
+  lazy val dusers = TableQuery[DUsers]
+
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,5 @@
-import java.sql.{DriverManager, Statement, Connection}
+import java.sql._
+import java.util.Date
 
 import scala.slick.jdbc.meta.MTable
 import scala.slick.driver.MySQLDriver.simple._
@@ -9,17 +10,39 @@ import scala.slick.driver.MySQLDriver.simple._
 object Main {
   def main(args: Array[String]): Unit = {
 
-    DAO.db.withSession(implicit sx => {
+    import DAO._
+    db.withSession(implicit sx => {
+
       if(MTable.getTables("users").list.isEmpty) {
-        DAO.users.ddl.create
+        users.ddl.create
+        val q = for(user <- users) yield user
+        if(q.countDistinct.run < 100) {
+          for(no <- 1 to 100) {
+            users += User(s"Jack Sparrow$no", s"jack$no@gmail.com",
+              s"$no Am Caption Jack Sparrow, Savvy", new Timestamp(new Date().getTime))
+          }
+        }
       }
+
+      if(MTable.getTables("dusers").list.isEmpty) {
+        dusers.ddl.create
+      }
+
     })
 
     var connection: Connection = null
-    var statement: Statement = null
+    var stmt: Statement = null
+
     try {
       connection = DriverManager.getConnection(Constants.dbURL, Constants.user, Constants.pass)
-
+      stmt = connection.createStatement()
+      val query = "select * from users"
+      val rs: ResultSet = stmt.executeQuery(query)
+      while(rs.next()) {
+        for(columnNo <- 0 until rs.getMetaData.getColumnCount) {
+          println(rs.getObject(columnNo).getClass)
+        }
+      }
     }
     catch {
       case ex: Exception =>
